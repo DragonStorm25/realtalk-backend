15dbed93365069a482138ff94e072818f59b4c3a
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-03 19:50:09 -0400

    Add basic LikeConcept class

--- server/concepts/likes.ts
+++ server/concepts/likes.ts
@@ -1,9 +1,13 @@
 import { ObjectId, IntegerType } from "mongodb";
 
-import { BaseDoc } from "../framework/doc";
+import DocCollection, { BaseDoc } from "../framework/doc";
 
 export interface LikeDoc extends BaseDoc {
   target: ObjectId;
   likes: IntegerType;
   dislikes: IntegerType;
+}
+
+export default class LikeConcept {
+  public readonly comments = new DocCollection<LikeDoc>("likes");
 }
