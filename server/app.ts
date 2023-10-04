fb4a1d6fe6a2f3d21b9e048685cf23edd3a9dcf0
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-03 20:36:09 -0400

    Add LikeConcept to app

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import PostConcept from "./concepts/post";
 import UserConcept from "./concepts/user";
 import WebSessionConcept from "./concepts/websession";
 import CommentConcept from "./concepts/comment";
+import LikeConcept from "./concepts/likes";
 
 // App Definition using concepts
 export const WebSession = new WebSessionConcept();
@@ -10,3 +11,4 @@ export const User = new UserConcept();
 export const Post = new PostConcept();
 export const Friend = new FriendConcept();
 export const Comment = new CommentConcept();
+export const Like = new LikeConcept();
