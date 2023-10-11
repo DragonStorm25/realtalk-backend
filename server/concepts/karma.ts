36b89e7f7d936474d4f8921ede042c001eb50c41
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-11 19:55:44 -0400

    Create and implement increaseKarma

--- server/concepts/karma.ts
+++ server/concepts/karma.ts
@@ -9,4 +9,14 @@ export interface KarmaDoc extends BaseDoc {
 
 export default class KarmaConcept {
   public readonly karma = new DocCollection<KarmaDoc>("karma");
+
+  async increaseKarma(user: ObjectId) {
+    const karma = await this.karma.readOne({ user });
+    if (karma) {
+      await this.karma.updateOne({ user }, { karmaAmount: karma.karmaAmount + 1 });
+    } else {
+      await this.karma.createOne({ user, karmaAmount: 1 });
+    }
+    return { msg: "Karma successfully increased!", comment: await this.karma.readOne({ user }) };
+  }
 }
