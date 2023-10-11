86da5abb75abbbb17d8650056a1c7f03ec7be5c9
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-11 19:51:13 -0400

    Create karma concept and set for karma storage

--- server/concepts/karma.ts
+++ server/concepts/karma.ts
@@ -1,8 +1,12 @@
 import { ObjectId } from "mongodb";
 
-import { BaseDoc } from "../framework/doc";
+import DocCollection, { BaseDoc } from "../framework/doc";
 
 export interface KarmaDoc extends BaseDoc {
   user: ObjectId;
   karmaAmount: number;
+}
+
+export default class KarmaConcept {
+  public readonly karma = new DocCollection<KarmaDoc>("karma");
 }
